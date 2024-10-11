10cd66c421949fd1012c3ec9c19c879252a09582
camille <chuayuen0122@gmail.com>
2024-10-11 16:32:12 +0800

    changed cart icon

--- frontend/components/cart/HeaderCart.tsx
+++ frontend/components/cart/HeaderCart.tsx
@@ -1,26 +1,14 @@
 import Link from 'next/link';
-import {useCart} from '../../hooks/cart';
-import clsx from 'clsx';
-import useFormatCurrency from '../../hooks/useFormatCurrency';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'; // Material UI Cart Icon
 
-export default function HeaderCart({className}: {className?: string}) {
-	const {total} = useCart();
-	const {formatCurrency} = useFormatCurrency();
-	const isEmpty = !total || !total.qty;
-	const isDoubleQty = (total?.qty && total?.qty > 9) ? true : false;
-
-	return (
-		<Link href={'/cart'}>
-			<a className={clsx('cart-header', {
-				'cart-header_empty': isEmpty,
-				'cart-header_active': !isEmpty
-			}, className)}>
-				<span className={'cart-header__icon'} />
-				<b className={clsx('cart-header__qty', {
-					'cart-header__qty_double': isDoubleQty
-				})}>{total?.qty ?? 0}</b>
-				<div className={'cart-header__total'}>{formatCurrency(total?.total || 0)}</div>
-			</a>
-		</Link>
-	);
+export default function HeaderCart() {
+  return (
+    <Link href={'/cart'}>
+      <a className="cart-header">
+        <div className="cart-header__icon-container">
+          <ShoppingCartIcon style={{ fontSize: 32 }} /> {/* Simple Cart Icon */}
+        </div>
+      </a>
+    </Link>
+  );
 }
